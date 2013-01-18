c0ecdea947d3040e0a3650e01b7efcd165279748
ydavid365 <ydavid365@gmail.com>
2013-01-17 17:42:00 -0800

    added separate staging api and base group scripts

--- versal-staging-api-base.py
+++ versal-staging-api-base.py
@@ -1,4 +1,4 @@
-#creates versal_cluster.map
+#creates versal-staging-api-base-compiled.map
 
 from mako.template import Template
 import yaml
@@ -6,12 +6,12 @@ import os
 
 #render mako|yaml
 #fp_ = '/Users/mini/Versal/cloudstate/versal-staging.map'
-fp_ = '/srv/cloudstate/versal-staging.map'
+fp_ = '/srv/cloudstate/versal-staging-api-base.map'
 temp_ = Template(open(fp_, 'r').read())
 map_ = temp_.render()
 
 #write to file:
 #f = open('/Users/mini/Versal/cloudstate/versal-staging-compiled.map', "w")
-f = open('/srv/cloudstate/versal-staging-compiled.map', "w")
+f = open('/srv/cloudstate/versal-staging-api-base-compiled.map', "w")
 f.write(map_)
 f.close()
---
+++ versal-staging-lb-base.py
@@ -0,0 +1,17 @@
+#creates versal-staging-lb-base-compiled.map
+
+from mako.template import Template
+import yaml
+import os
+
+#render mako|yaml
+#fp_ = '/Users/mini/Versal/cloudstate/versal-staging.map'
+fp_ = '/srv/cloudstate/versal-staging-lb-base.map'
+temp_ = Template(open(fp_, 'r').read())
+map_ = temp_.render()
+
+#write to file:
+#f = open('/Users/mini/Versal/cloudstate/versal-staging-compiled.map', "w")
+f = open('/srv/cloudstate/versal-staging-lb-base-compiled.map', "w")
+f.write(map_)
+f.close()
