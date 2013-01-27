73600e97750a90d37b166e07e59b8086611509ce
Alexy Khrabrov <alexy.khrabrov@gmail.com>
2013-01-26 17:31:39 -0800

    name instances with region, pick cloud.profiles with a symlink

--- uber.py
+++ uber.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -45,6 +46,8 @@ def load_pillar():
 def name_instance_kind(env, instance_kind, region_index, subregion_index):
   return "%s_%s_region-%d-%d" % (env, instance_kind, region_index, subregion_index)
 
+def name_instance(name, region_index, subregion_index, domain):
+  return "%s-region-%d-%d-%s" % (name, region_index, subregion_index, domain)
 
 def generate_cloud_profiles(p, envs, os_name="Ubuntu", os_version="12_04_LTS"):
   r = {}
@@ -76,29 +79,30 @@ def generate_cloud_profiles(p, envs, os_name="Ubuntu", os_version="12_04_LTS"):
   return r
 
 
-def generate_role(p, role, env):
+def generate_role(p, role):
   server_group = p['server_roles'][role]
   server_names = p['server_names'][role]
   r = {}
   instance_count = 0
   for instance in server_group['instances']:
+    environment     = p['environment']
     instance_size   = server_group['size']
     region_index    = instance['region']
     subregion_index = instance['subregion']
 
-    instance_kind = name_instance_kind(env, instance_size, region_index, subregion_index)
+    instance_kind = name_instance_kind(environment, instance_size, region_index, subregion_index)
 
     instance_list = []
 
     for i in range(instance['number']):
-      instance_name = "%s-%s-%d-%d-%s" % (server_names[instance_count], env, region_index, subregion_index, p['domain'])
+      instance_name = name_instance(server_names[instance_count], region_index, subregion_index, p['domain'])
 
       instance_props = {
         'master': p['salt_master'],
         'grains': {
           'roles': [server_group['role']]
           }, 
-        'environment': env # TODO should we store and retrieve it from p['env']?
+        'environment': environment # TODO should we store and retrieve it from p['env']?
       }
 
       instance_list.append({instance_name: instance_props})
@@ -131,7 +135,7 @@ def main():
     print dump(r, default_flow_style=False)
   elif args.role:
     print >>sys.stderr, "generating the %s roles map" % args.role
-    r = generate_role(p, args.role, args.env)
+    r = generate_role(p, args.role)
     print dump(r, default_flow_style=False)
   else:
     print "you want something which doesn't seem to exist, eh?"
