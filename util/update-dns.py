1ecd6c9736911c37f1aa4f3fb938e31b32eea36e
Alexy Khrabrov <alexy.khrabrov@gmail.com>
2013-01-27 20:18:34 -0800

    update-dns works properly

--- util/update-dns.py
+++ util/update-dns.py
@@ -33,21 +33,33 @@ def load_running_status(status_file, provider, box_pattern):
 
 
 def query_running_dns(records, zone):
-	for name, ips in records:
-		assigned = route53.query(zone, name)
-		print >>sys.stderr, "%s => " % name, assigned
-		if not assigned:
-		  just_assigned = route53.assign(zone, name, ips[0], 60)
-		  check_assigned = route53.query(zone, name)
-		  print >>sys.stderr, "just assigned %s => " % name, just_assigned, check_assigned
+  for rec in records:
+    assigned = route53.query(zone, rec)
+    print >>sys.stderr, "%s => " % rec, assigned
 
 
-def main():
+# TODO this only works for amazon
+def aws_public_dns_from_ip(ip, location):
+	# ec2-54-245-178-203.us-west-2.compute.amazonaws.com
+	return "ec2-%s.%s.compute.amazonaws.com" % (re.sub('\.','-',ip), location)
+
+def ensure_running_dns(records, zone):
+  for rec in records:
+	# TODO place the default TTL somewhere central
+	host = records[rec]
+	route53.assign(zone, rec, aws_public_dns_from_ip(host['public_ip'],host['location']), 60)
+	assigned = route53.query(zone, rec)
+	print >>sys.stderr, "just assigned %s => " % rec, assigned
+
+
+def __main__():
   parser = argparse.ArgumentParser(description='Versal DNS updater')
   parser.add_argument('--provider', default="aws", help="cloud provider id recognized by libcloud")
   parser.add_argument('--domain', default="vrsl.net", help="domain which must be contained in the instances names")
   parser.add_argument('--env', default='staging', help="environment which must be contained in the instance names")
   parser.add_argument('status_file', help="the salt-cloud -Q yaml output file")
+  parser.add_argument('-e', '--ensure', action="store_true", help="when supplied, actually assign everything")
+  parser.add_argument('-q', '--query',  action="store_true", help="when supplied, query DNS for the running instances")
   arg = parser.parse_args()
 
   box_pattern = ".*%s.%s" % (arg.env, arg.domain)
@@ -57,6 +69,12 @@ def main():
   records = load_running_status(arg.status_file, arg.provider, box_pattern)
   print >>sys.stderr, "working with records ", records
 
-  #query_running_dns(records, arg.domain)
+  if arg.query:
+  	query_running_dns(records, arg.domain)
 
-main()
+  if arg.ensure:
+  	ensure_running_dns(records, arg.domain)
+
+
+#if __name__ == __main__:
+__main__()
